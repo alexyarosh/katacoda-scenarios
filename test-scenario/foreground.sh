4e3e8039d768a27b1a2afbb97fc46f3d2c797c9d
Alex Yarosh <yarosh@cockroachlabs.com>
2021-03-23 21:23:13 -0400

    Update foreground.sh

--- test-scenario/foreground.sh
+++ test-scenario/foreground.sh
@@ -1,3 +1,4 @@
-wget -qO- https://binaries.cockroachdb.com/cockroach-v20.2.6.linux-amd64.tgz | tar  xvz
-cp -i cockroach-v20.2.6.linux-amd64/cockroach /usr/local/bin/
-cockroach start-single-node --insecure
+echo "Hi"
+#wget -qO- https://binaries.cockroachdb.com/cockroach-v20.2.6.linux-amd64.tgz | tar  xvz
+#cp -i cockroach-v20.2.6.linux-amd64/cockroach /usr/local/bin/
+#cockroach start-single-node --insecure
